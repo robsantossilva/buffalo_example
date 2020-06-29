14eb46457670db8adb89439c620ae8c03cd5c2ee
Robson Silva <robsantossilva@gmail.com>
2020-06-29 19:37:08 -0300

    fist commit

---
+++ models/category.go
@@ -0,0 +1,54 @@
+package models
+
+import (
+	"encoding/json"
+	"github.com/gobuffalo/nulls"
+	"github.com/gobuffalo/pop/v5"
+	"github.com/gobuffalo/validate/v3"
+	"github.com/gofrs/uuid"
+	"time"
+	"github.com/gobuffalo/validate/v3/validators"
+)
+// Category is used by pop to map your categories database table to your go code.
+type Category struct {
+    ID uuid.UUID `json:"id" db:"id"`
+    Name string `json:"name" db:"name"`
+    Description nulls.String `json:"description" db:"description"`
+    CreatedAt time.Time `json:"created_at" db:"created_at"`
+    UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+}
+
+// String is not required by pop and may be deleted
+func (c Category) String() string {
+	jc, _ := json.Marshal(c)
+	return string(jc)
+}
+
+// Categories is not required by pop and may be deleted
+type Categories []Category
+
+// String is not required by pop and may be deleted
+func (c Categories) String() string {
+	jc, _ := json.Marshal(c)
+	return string(jc)
+}
+
+// Validate gets run every time you call a "pop.Validate*" (pop.ValidateAndSave, pop.ValidateAndCreate, pop.ValidateAndUpdate) method.
+// This method is not required and may be deleted.
+func (c *Category) Validate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.Validate(
+		&validators.StringIsPresent{Field: c.Name, Name: "Name"},
+	), nil
+}
+
+// ValidateCreate gets run every time you call "pop.ValidateAndCreate" method.
+// This method is not required and may be deleted.
+func (c *Category) ValidateCreate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// ValidateUpdate gets run every time you call "pop.ValidateAndUpdate" method.
+// This method is not required and may be deleted.
+func (c *Category) ValidateUpdate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
